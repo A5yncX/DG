02206075c98f3df0cc99828cb7ca328fab50dd13
AsyncX <1158271328@qq.com>
2024-02-27 13:18:52 +0800

    修改site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://demo.asyncx.top',
+  site: 'https://blog.asyncx.top',
   integrations: [
 	sitemap(), 
 	]
