9d79b0f3030f1a2afb65c0296e7ebeee75189811
AsyncX <1158271328@qq.com>
2024-03-06 17:39:58 +0800

    change en rss add img

--- src/pages/en.xml.js
+++ src/pages/en.xml.js
@@ -4,21 +4,21 @@ import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
-  // 获取中文博客内容集合
+  // 获取en博客内容集合
   export async function GET(context) {
     const blog = await getCollection('blog', ({ id }) => id.startsWith('en/'));
     blog.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
     return rss({
       title: 'AsyncX’s Blog',
-      stylesheet: 'css/pretty-feed-v3.xsl',
       description: 'i18n/Content Aggregation/Programming/Philosophy/Hobbies',
       site: context.site,
       items: blog.map((post) => ({
         link: `/en/blog/${post.title}/`,
         // 注意：这不会处理 MDX 文件中的组件或 JSX 表达式。
-        content: sanitizeHtml(parser.render(post.body)),
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        }),
         ...post.data,
-      })
-      ),
+      })),
     });
   }
--- src/pages/zh.xml.js
+++ src/pages/zh.xml.js
@@ -11,9 +11,8 @@ const parser = new MarkdownIt();
     blog.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
     return rss({
       title: 'AsyncX | 嘿!我是AX!',
-      // stylesheet: 'css/pretty-feed-v3.xsl',
       description: 'AsyncX的博客-i18n多语言/内容聚合/编程/哲学/爱好',
-      image: '/favicon.svg',
+      // image: '/favicon.svg',
       site: context.site,
       items: blog.map((post) => ({
         title: post.data.title,
