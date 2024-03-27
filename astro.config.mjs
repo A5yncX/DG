cd7ab22a086b25d7539396a281472528ec902e6e
AsyncX <1158271328@qq.com>
2024-03-27 17:12:46 +0800

    done.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import remarkFigureCaption from '@microflash/remark-figure-caption';
 import remarkDirective from 'remark-directive';
 import remarkCalloutDirectives from "./src/components/mdrenders/remark-callout-directives-customized.js"
 import { remarkReadingTime } from './src/components/mdrenders/remark-reading-time.mjs';
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
 import sitemap from '@astrojs/sitemap';
 
 import expressiveCode from "astro-expressive-code";
@@ -12,6 +13,7 @@ export default defineConfig({
   site: 'https://blog.asyncx.top',
   integrations: [sitemap(), 
     expressiveCode({
+      plugins: [pluginCollapsibleSections()],
       themes: ['material-theme-ocean'],
       styleOverrides: {
         // You can also override styles
